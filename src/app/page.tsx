f4ff3a4b0a226eb71407fd5cc798863f8729d22c
iamdrzazgowski <iamdrzazgowski@gmail.com>
2026-04-12 14:18:21 +0200

    fix padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import About from "@/components/about";
-import Experience from "@/components/experience";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import Projects from "@/components/projects";
-import SectionSeparator from "@/components/section-separator";
-import Stack from "@/components/stack";
+import About from '@/components/about';
+import Experience from '@/components/experience';
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+import Projects from '@/components/projects';
+import SectionSeparator from '@/components/section-separator';
+import Stack from '@/components/stack';
 
 export default function Home() {
     return (
-        <main className="max-w-[720px] mx-auto px-6 py-10 pb-16 text-[17px] leading-[1.75]">
+        <main className='max-w-[720px] mx-auto px-6 py-10 pb-16 md:pt-24 text-[17px] leading-[1.75]'>
             <Header />
-            <SectionSeparator label="about" />
+            <SectionSeparator label='about' />
             <About />
-            <SectionSeparator label="tech stack" />
+            <SectionSeparator label='tech stack' />
             <Stack />
             {/* <SectionSeparator label='experience' /> */}
             <Experience />
-            <SectionSeparator label="projects" />
+            <SectionSeparator label='projects' />
             <Projects />
             <Footer />
         </main>
