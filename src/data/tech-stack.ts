5479727f56c2506cc04467999acfa40b1479c15a
iamdrzazgowski <iamdrzazgowski@gmail.com>
2026-05-23 16:40:21 +0200

    update techstack

--- src/data/tech-stack.ts
+++ src/data/tech-stack.ts
@@ -26,6 +26,7 @@ export const stackData: StackCategory[] = [
             'Express',
             'Socket.IO',
             'PostgreSQL',
+            'MongoDB'
             'Prisma ORM',
             'Supabase',
             'Better Auth',
